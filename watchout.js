346e7ac99a854ff54b5b33bb743e10390a1d4b62
MKS-Student <mks.student@hackreactor.com>
2015-03-13 19:42:32 -0700

    Added Collision Detection and Scoring.

--- watchout.js
+++ watchout.js
@@ -1,11 +1,14 @@
 //initialize
 //game container
-var canvasSize = {'x' : 1000, 'y' : 1000}
+var canvasSize = {'x' : 500, 'y' : 500}
 var gameCanvas = 
 	d3.select(".gameCanvas")
 	  .attr('width', canvasSize.x)
 	  .attr('height', canvasSize.y);
-
+var score = 0;
+var highScore = 0;
+d3.select('.hero').attr('cx', canvasSize.x/2);
+d3.select('.hero').attr('cy', canvasSize.y/2);
 
 //ENEMY STUFF
 //number of enemies
@@ -31,6 +34,38 @@ var enemyUpdate = function(data){
       .duration(2000)
 	  	.attr('cx', function(d){ return d.x})
 	  	.attr('cy', function(d){ return d.y});		
+
+
+  //COLLISION STUFF
+  var checkCollision = function(data){
+
+    d3.select('.current span').text(score);
+    d3.select('.high span').text(highScore);
+    if(score > highScore){
+      highScore = score;
+    }
+
+  	enemies.each(function() {
+  	  var heroX = parseFloat(d3.select('.hero').attr('cx'));
+  	  var heroY = parseFloat(d3.select('.hero').attr('cy'));
+  	  var heroR = parseFloat(d3.select('.hero').attr('r'));
+  	  var enemyX = parseFloat(d3.select(this).attr('cx'));
+  	  var enemyY = parseFloat(d3.select(this).attr('cy'));
+  	  var enemyR = parseFloat(d3.select(this).attr('r'));
+
+  	  var dx = (heroX + heroR) - (enemyX + enemyR);
+  	  var dy = (heroY + heroR) - (enemyY + enemyR);
+  	  var distance = Math.sqrt(dx * dx + dy * dy);
+ 
+      //console.log("distance: " + distance)
+  	  if (distance < (heroR + enemyR)){
+        score = 0;
+  	  }
+  	});
+  }
+
+  setInterval( checkCollision , 100);
+
 }
 
 //create the enemy data array
@@ -54,7 +89,7 @@ var enemyArray = function(enemyNumber){
 var dragmove = function() {
   d3.select(this)
     .attr('cx', d3.event.sourceEvent.pageX-10)
-    .attr('cy', d3.event.sourceEvent.pageY-55);
+    .attr('cy', d3.event.sourceEvent.pageY-45);
 }
 var drag = d3.behavior.drag()
     .on("drag", dragmove);
@@ -65,4 +100,7 @@ d3.select('.hero').call(drag);
 //run enemy updater passing in enemy array
 enemyUpdate(enemyArray(enemyNum));
 
+setInterval( function(){
+	score += 12547;
+} , 100);
 setInterval( function(){enemyUpdate(enemyArray(enemyNum))} , 1000);
